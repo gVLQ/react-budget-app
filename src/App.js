d19f0671302dbb9cf3815cb9afc47307c80a4823
Vlaicu, Gabriel <Gabriel.Vlaicu@finastra.com>
2019-07-24 16:15:33 +0300

    work done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import ExpenseList from "./components/ExpenseList";
 import ExpenseForm from "./components/ExpenseForm";
 import Alert from "./components/Alert";
-// Import random id generation dependency
 import uuid from 'uuid/v4'
 
-const initialExpenses = [
-  {
-    id: uuid(),
-    charge: "rent",
-    amount: 1000
-  },
-  {
-    id: uuid(),
-    charge: "car",
-    amount: 500
-  },
-  {
-    id: uuid(),
-    charge: "shop",
-    amount: 300
-  }
-];
+const initialExpenses = localStorage.getItem('expenses') ? JSON.parse(localStorage.getItem('expenses')) : [];
 
 function App() {
   // State Values
@@ -31,6 +14,21 @@ function App() {
   const [charge, setCharge] = useState('');
   // Single Amount
   const [amount, setAmount] = useState('');
+  // Alert
+  const [alert, setAlert] = useState({
+    show: false
+  });
+  // Edit
+  const [edit, setEdit] = useState(false);
+  // Edit Item
+  const [id, setId] = useState(0);
+
+  // useEffect
+  useEffect(() => {
+    console.log('useEffect called');
+    localStorage.setItem('expenses', JSON.stringify(expenses))
+  }, [expenses])
+
   // Functionality
   const handleCharge = e => {
     setCharge(e.target.value);
@@ -42,22 +40,77 @@ function App() {
 
   const handleSubmit = e => {
     e.preventDefault();
+    if (charge !== '' &&  amount > 0) {
+      if (edit) {
+        let tempExpenses = expenses.map(item => {
+          return item.id === id ? {...item, charge, amount} : item
+        });
+        setExpenses(tempExpenses);
+        setEdit(false);
+        handleAlert({type:'success', text:'Item Edited!'});
+      } else {
+        const singleExpense = {
+          id: uuid(),
+          charge,
+          amount
+        };
+        setExpenses([...expenses, singleExpense]);
+        handleAlert({type:'success', text:'Item Added!'});
+      }
+      setCharge('');
+      setAmount('');
+    } else {
+      handleAlert({type:'danger', text:`Charge can't be empty value and amount must be greater than 0!`});;
+    }
+  }
+
+  const handleAlert = ({type, text}) => {
+    setAlert({show:true, type, text});
+    setTimeout(() => {
+      setAlert({show:false})
+    }, 4000);
+  }
+
+  const clearItems = () => {
+    setExpenses([]);
+    handleAlert({type: "danger", text: "All Items Deleted!"});
+  };
+
+  const handleDelete =(id) => {
+    let tempExpenses = expenses.filter( (item) => {
+      return item.id !== id;
+    });
+    setExpenses(tempExpenses);
+    handleAlert({type: "danger", text: "Item Deleted!"});
+  }
+
+  const handleEdit =(id) => {
+    let expense = expenses.find(item => item.id === id);
+    let {charge, amount} = expense;
+    setCharge(charge);
+    setAmount(amount);
+    setEdit(true);
+    setId(id);
   }
 
   return (
     <>
-      <Alert />
+      {alert.show && <Alert type={alert.type} text={alert.text}/>}
       <h1>Vlq's Budget Calculator</h1>
       <main className="App">
         <ExpenseForm charge={charge} 
                      amount={amount} 
                      handleAmount={handleAmount} 
                      handleCharge={handleCharge} 
-                     handleSubmit={handleSubmit}/>
-        <ExpenseList expenses={expenses} />
+                     handleSubmit={handleSubmit}
+                     edit={edit}/>
+        <ExpenseList expenses={expenses} 
+                     handleDelete={handleDelete}
+                     handleEdit={handleEdit}
+                     clearItems={clearItems}/>
       </main>
       <h1>
-        Total Spending: <span className="total">$ {expenses.reduce( (acc, curr) => { return (acc += curr.amount) }, 0)}</span>
+        Total Spending: <span className="total">$ {expenses.reduce( (acc, curr) => { return (acc += parseInt(curr.amount)) }, 0)}</span>
       </h1>
     </>
   );
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const Alert = () => {
-    return (
-        <div>
-            Hello from Alert!
-        </div>
-    )
+const Alert = ({type, text}) => {
+    return <div className={`alert alert-${type}`}>
+        {text}
+    </div>
 }
 
 export default Alert
